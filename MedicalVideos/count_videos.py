e2bcf9a75c7b0f1bf55f2b4d9836415aea0cb1c9
diego ibarra <diegoarrabi1@gmail.com>
2025-07-05 09:46:50 -0400

    i have no idea how to recursively iterate through a dir

--- MedicalVideos/count_videos.py
+++ MedicalVideos/count_videos.py
@@ -1,16 +1,58 @@
+import os
+import subprocess
 
 
-file = "/Users/diegoibarra/Media/Medical/BoardsBeyond/video.txt"
+class Item:
+    def __init__(self, path: str):
+        self.isdir: bool = os.path.isdir(path)
+        self.isfile: bool
+        self.path = path
+        self.item_name: str = os.path.basename(path).upper()
+        self.contents: list
 
-with open(file, "r") as openfile:
-    lineslist = openfile.readlines()
+    def dirContents(self):
+        self.isfile = not self.isdir
+        if self.isdir:
+            self.contents = [os.path.join(self.path, name) for name in os.listdir(self.path)
+                             if not name.startswith(".") and not name.lower().endswith(".pdf")]
+    
+    
+    
 
-# for i in lineslist:
-#     print(i)
 
-videos_list = [line for line in lineslist if line.rstrip().endswith(".mp4")]
+class VideoTopic:
+    def __init__(self, directory: str):
+        self.path = directory
+        self.topic_name: str = os.path.basename(directory).upper()
+        self.contents: list[Item] = []
+        self.dirContents()
 
-# for i in videos_list:
-#     print(i, end="")
+    def dirContents(self):
+        content_paths = [os.path.join(self.path, name) for name in os.listdir(self.path)
+                         if not name.startswith(".") and not name.lower().endswith(".pdf")]
+        for item in content_paths:
+            self.contents.append(Item(item))
+        
 
-print(len(videos_list))
+    def iterDir(self, path: Item):
+        if path.isfile: 
+            print("IS FILE")
+        
+
+    def printContents(self):
+        print(self.topic_name)
+
+
+
+
+PARENT_DIRECTORY = os.path.join(os.path.dirname(__file__), "resources", "videos")
+topics_list = [os.path.join(PARENT_DIRECTORY, path) for path in os.listdir(PARENT_DIRECTORY) if not path.startswith(".")]
+
+all_topics_list: list[VideoTopic] = []
+
+for topic_path in topics_list:
+    all_topics_list.append(VideoTopic(topic_path))
+
+for item in all_topics_list:
+    print(item.topic_name)
+    item.printContents()
